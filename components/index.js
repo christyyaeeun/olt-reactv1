18ad63396e975160f2b0902ba68582d6c25d3960
christyyaeeun <46875152+christyyaeeun@users.noreply.github.com>
2022-03-11 01:34:05 -0600

    Create index.js

---
+++ components/index.js
@@ -0,0 +1,18 @@
+
+
+export { default as Navigation } from "./Navigation";
+export { default as Footer } from "./Navigation/Footer";
+export { default as Home } from "./Home";
+export { default as UserProfile } from "./UserProfile";
+export { default as Settings } from "./Settings";
+export { default as Together } from "./Together";
+export { default as Journal } from "./Journal";
+export { default as Entry } from "./Entry";
+export { default as Timeline } from "./Timeline";
+export { default as Feed } from "./journal/Feed";
+export { default as Posts } from "./journal/Posts";
+export { default as Post } from "./journal/Post";
+// export * from './OLTCalendar';
+// export { default as OLTCalendar } from "./OLTCalendar";
+// export { default as Menu } from "./Menu";
+
